ed0d15e5109f70c8ed03b73d194568556707d5ac
Karl Xu <karl.yogi.xu@gmail.com>
2018-08-27 01:51:54 +0800

    responding message inside Coderbunker DevOps Team

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { Wechaty } = require('wechaty') // import { Wechaty } from 'wechaty'
 
-Wechaty.instance() // Global Instance
+const bot = Wechaty.instance() // Global Instance
 .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrcode)}`))
 .on('login',            user => console.log(`User ${user} logined`))
 .on('message',       message => console.log(`Message: ${message}`))
+.on('message', async message => {
+    const room = message.room()
+    console.log(room)
+    const topic = await room.topic()
+    console.log(topic)
+    console.log(message.text())
+    if("Coderbunker DevOps Team" == topic && "test" == message.text()) {
+        message.room().say("Bot responding.")
+    }
+})
 .start()
